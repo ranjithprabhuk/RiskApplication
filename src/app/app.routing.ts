94196febc25c7d2b1491c82e2fb46e0e94c10ed3
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2017-06-19 03:14:32 +0530

    routes are added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Layouts
+import { FullLayoutComponent } from './layouts/full-layout.component';
 
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'settle',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'settle',
+        loadChildren: './settle/settle.module#SettleModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
 export class AppRoutingModule {}
