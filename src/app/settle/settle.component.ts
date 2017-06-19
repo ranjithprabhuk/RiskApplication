b689abe9a4a950173476fb83cbfa20c7cb90d5f7
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2017-06-19 10:39:57 +0530

    empty cells are avoided

--- src/app/settle/settle.component.ts
+++ src/app/settle/settle.component.ts
@@ -2,100 +2,102 @@ import { Component, OnInit } from '@angular/core';
 import { SettleService } from './settle.service';
 
 @Component({
-  templateUrl: 'settle.component.html',
-  providers: [SettleService]
+    templateUrl: 'settle.component.html',
+    providers: [SettleService]
 })
 export class SettleComponent {
 
-  constructor(private settleService: SettleService) { }
+    constructor(private settleService: SettleService) { }
 
-  public settledList: Array<Object> = [];
-  public barChartOptions: Object = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public customerList: string[] = [];
-  public barChartType = 'bar';
-  public barChartLegend = true;
+    public settledList: Array<Object> = [];
+    public barChartOptions: Object = {
+        scaleShowVerticalLines: false,
+        responsive: true
+    };
+    public customerList: string[] = [];
+    public barChartType = 'bar';
+    public barChartLegend = true;
 
-  public barChartData: any[] = [
-    { data: [], label: 'Stake' },
-    { data: [], label: 'Win' }
-  ];
-
-
-  //get the list of settle data from the api
-  public getSettledBetData(): void {
-
-    //call the settle service to get the data
-    this.settleService.getSettleData().then(res => {
-      this.settledList = this.groupedCustomerData(res);
-    }).catch(err => { console.log("Error: ", err); });
-  };
+    public barChartData: any[] = [
+        { data: [], label: 'Stake' },
+        { data: [], label: 'Win' }
+    ];
 
 
-  //convert the csv data into a Json Data and also group it based on Customer
-  public groupedCustomerData(csv: string): Array<Object> {
+    //get the list of settle data from the api
+    public getSettledBetData(): void {
 
-    let lines = csv.split("\n");
-    let result = {};
-    let groupedData = [];
+        //call the settle service to get the data
+        this.settleService.getSettleData().then(res => {
+            this.settledList = this.groupedCustomerData(res);
+        }).catch(err => { console.log("Error: ", err); });
+    };
 
-    //first line is a header so split the header 
-    let headers = lines[0].split(",");
 
-    result = this.convertCsvToJson(headers, lines);
+    //convert the csv data into a Json Data and also group it based on Customer
+    public groupedCustomerData(csv: string): Array<Object> {
 
-    //convert the object into an Array
-    for (let i in result) {
-      //Win String Fix
-      result[i].Win = result[i][headers[4]];
+        let lines = csv.split("\n");
+        let result = {};
+        let groupedData = [];
 
-      //calculate the risk
-      result[i].isRisky = this.calculateRisk(result[i]);
-      this.barChartData[0].data.push(result[i].Stake);
-      this.barChartData[1].data.push(result[i].Win);
-      this.customerList.push(result[i].Customer);
-      groupedData.push(result[i]);
-    }
+        //first line is a header so split the header 
+        let headers = lines[0].split(",");
 
-    return groupedData; //JSON
-  };
+        result = this.convertCsvToJson(headers, lines);
 
-  //convert the CSV data into the JSON data
-  public convertCsvToJson(headers: string[], lines: string[]): Object {
+        //convert the object into an Array
+        for (let i in result) {
+            //Win String Fix
+            result[i].Win = result[i][headers[4]];
 
-    let result = {};
-    //iterate the remaining values and construct the JSON Array
-    for (let i = 1; i < lines.length; i++) {
-
-      let currentline = lines[i].split(",");
-      let customer = currentline[0];
-
-      if (result && result[customer]) {
-        result[customer][headers[3]] += parseInt(currentline[3]);
-        result[customer][headers[4]] += parseInt(currentline[4]);
-      } else {
-        //create a new object in the result object
-        result[customer] = {};
-
-        //iterate the single line and contruct the JSON object
-        for (let j = 0; j < headers.length; j++) {
-          result[customer][headers[j]] = parseInt(currentline[j]);
+            //calculate the risk
+            result[i].isRisky = this.calculateRisk(result[i]);
+            this.barChartData[0].data.push(result[i].Stake);
+            this.barChartData[1].data.push(result[i].Win);
+            this.customerList.push(result[i].Customer);
+            groupedData.push(result[i]);
         }
-      }
+
+        return groupedData; //JSON
+    };
+
+    //convert the CSV data into the JSON data
+    public convertCsvToJson(headers: string[], lines: string[]): Object {
+
+        let result = {};
+        //iterate the remaining values and construct the JSON Array
+        for (let i = 1; i < lines.length; i++) {
+
+            let currentline = lines[i].split(",");
+            let customer = currentline[0];
+            if (!isNaN(parseInt(customer))) {
+                if (result && result[customer]) {
+                    result[customer][headers[3]] += parseInt(currentline[3]);
+                    result[customer][headers[4]] += parseInt(currentline[4]);
+                } else {
+                    //create a new object in the result object
+                    result[customer] = {};
+
+                    //iterate the single line and contruct the JSON object
+                    for (let j = 0; j < headers.length; j++) {
+                        result[customer][headers[j]] = parseInt(currentline[j]);
+                    }
+                }
+            }
+
+        }
+
+        return result;
+    };
+
+    //calculate the risk
+    public calculateRisk(data: any): boolean {
+        return data.Stake / 100 * 60 < data.Win ? true : false;
     }
 
-    return result;
-  };
 
-  //calculate the risk
-  public calculateRisk(data: any): boolean {
-    return data.Stake / 100 * 60 < data.Win ? true : false;
-  }
-
-
-  ngOnInit(): void {
-    this.getSettledBetData();
-  }
+    ngOnInit(): void {
+        this.getSettledBetData();
+    }
 }
